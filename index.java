ea39fe40c116a4db11b8385eef6941edc07ce51b
Pankaj Choudhary <coolpankaj954@gmail.com>
2024-11-27 00:22:00 +0530

    initial commit

---
+++ index.java
@@ -0,0 +1,9 @@
+class index{
+    public static void main(String[]args){
+        // Sum of two Numbers a and b
+        int a = 10;
+        int b = 30;
+        int sum = a+b;
+        System.out.println("The sum of two number is:"+sum);
+    }
+}
